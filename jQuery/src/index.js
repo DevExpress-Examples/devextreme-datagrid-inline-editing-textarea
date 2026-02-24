939d3311ae5641104b8280898cdb8593d0b09af7
rodgedeluna-ds <rodge.deluna@devsoft.ph>
2026-02-24 16:48:34 +0800

    Add jQuery example

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,12 +1,72 @@
-import { count as initialCount } from "./data.js";
+import { employees, states } from "./data.js";
 
 $(() => {
-  let count = initialCount;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
-    },
-  });
+const employeesStore = new DevExpress.data.ArrayStore({
+		key: "ID",
+		data: employees
+	});
+
+	const dataGrid = $("#gridContainer")
+		.dxDataGrid({
+			dataSource: employeesStore,
+			showBorders: true,
+			paging: {
+				enabled: false
+			},
+			editing: {
+				mode: "cell",
+				allowUpdating: true
+			},
+			columns: [
+				{
+					dataField: "Prefix",
+					caption: "Title",
+					width: 80
+				},
+				"FirstName",
+				"LastName",
+				{
+					dataField: "Position",
+					width: 170
+				},
+				{
+					dataField: "StateID",
+					caption: "State",
+					width: 125,
+					lookup: {
+						dataSource: states,
+						displayExpr: "Name",
+						valueExpr: "ID"
+					}
+				},
+				{
+					dataField: "Notes",
+					editorType: "dxTextArea",
+					width: 300,
+					editorOptions: {
+						elementAttr: {
+							class: "custom-textarea-class"
+						},
+						autoResizeEnabled: true,
+						onInput(args) {
+							var el = args.element[0];
+							if (el.prevClientHeight !== el.clientHeight) {
+								dataGrid.updateDimensions();
+							}
+							el.prevClientHeight = el.clientHeight;
+						}
+					},
+					cellTemplate: function (element, info) {
+						$("<div>")
+							.appendTo(element)
+							.text(info.value)
+							.css("width", info.column.width - 20)
+							.css("height", "auto")
+							.css("white-space", "normal")
+							.css("overflow-wrap", "break-word");
+					}
+				}
+			]
+		})
+		.dxDataGrid("instance");
 });
