f4056c3148da47b12ef44ad2b6dfbdac3d9607b0
rodgedeluna-ds <rodge.deluna@devsoft.ph>
2026-03-06 17:21:23 +0800

    Add React sample

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,17 +1,71 @@
-import { useCallback, useState } from 'react';
+import { useMemo, useRef } from 'react';
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import DataGrid, { Editing, Paging, Column, Lookup } from 'devextreme-react/data-grid';
+import type { DataGridRef, DataGridTypes } from 'devextreme-react/data-grid';
+import TextArea from 'devextreme-react/text-area';
+import type { TextAreaTypes } from 'devextreme-react/text-area';
+import ArrayStore from 'devextreme/data/array_store';
+import { employees, states } from './data';
+
+type TextAreaElement = HTMLElement & {
+  prevClientHeight?: number;
+};
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
+  const grid = useRef<DataGridRef>(null);
+
+  const employeesStore = useMemo(
+    () =>
+      new ArrayStore({
+        key: 'ID',
+        data: employees,
+      }),
+    [],
+  );
+
+  const notesEditorRender = (cell: DataGridTypes.ColumnEditCellTemplateData) => {
+    const onValueChanged = (e: TextAreaTypes.ValueChangedEvent) => cell.setValue(e.value);
+
+    const onNotesInput = (args: TextAreaTypes.InputEvent) => {
+      const el = args.element as TextAreaElement;
+      if (el.prevClientHeight !== el.clientHeight) {
+        grid.current?.instance().updateDimensions();
+      }
+      el.prevClientHeight = el.clientHeight;
+    };
+
+    return (
+      <TextArea
+        defaultValue={cell.value}
+        onValueChanged={onValueChanged}
+        elementAttr={{
+          class: 'custom-textarea-class',
+        }}
+        autoResizeEnabled={true}
+        onInput={onNotesInput}
+      />
+    );
+  };
+
+  const notesCellRender = (cellInfo: DataGridTypes.ColumnCellTemplateData) => (
+    <div className='notes-cell-content'>{cellInfo.value}</div>
+  );
+
   return (
-    <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
-    </div>
+    <DataGrid ref={grid} dataSource={employeesStore} showBorders={true}>
+      <Paging enabled={false} />
+      <Editing mode='cell' allowUpdating={true} />
+
+      <Column dataField='Prefix' caption='Title' width={80} />
+      <Column dataField='FirstName' />
+      <Column dataField='LastName' />
+      <Column dataField='Position' width={170} />
+      <Column dataField='StateID' caption='State' width={125}>
+        <Lookup dataSource={states} displayExpr='Name' valueExpr='ID' />
+      </Column>
+      <Column dataField='Notes' width={300} editCellRender={notesEditorRender} cellRender={notesCellRender} />
+    </DataGrid>
   );
 }
 
